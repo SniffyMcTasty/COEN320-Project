130d59dbd8a6ebc4dcf429d0279e35448cc97680
Mohammed Al-Taie <altaiem888@gmail.com>
2022-11-18 22:50:06 -0500

    push the new files plane common and main

---
+++ src/Plane.h
@@ -0,0 +1,46 @@
+/*
+ * Plane.h
+ *
+
+ */
+
+#ifndef PLANE_H_
+#define PLANE_H_
+
+#include "common.h"
+class Plane
+{
+    friend ostream &operator<<(ostream &out, const Plane &plane);
+    friend void *planeThread(void *arg);
+
+private:
+    PlaneInfo info;
+    pthread_t thread;
+    string channel;
+    name_attach_t *attach;
+    int coid;
+    timer_t timerId;
+
+    void setup();
+    void setupChannel();
+    void setupTimer();
+    void destroy();
+    void destroyTimer();
+    void destroyChannel();
+    void updatePosition();
+
+public:
+    Plane(PlaneInfo info);
+    int join();
+    bool inZone();
+    int ping();
+    int radarReply(_Int32t scoid);
+
+    static PlaneInfo randomInfo();
+};
+
+
+
+
+
+#endif /* PLANE_H_ */
